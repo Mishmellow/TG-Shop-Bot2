503c582f4be07a4ce5ada03a8a1d6450a22e78ff
Misha <guglmit599@gmail.com>
2025-11-01 13:49:01 +0200

    Fix line 102

--- Handlers/order.py
+++ Handlers/order.py
@@ -99,7 +99,7 @@ async def confirm_order(callback: CallbackQuery, state: FSMContext, bot: Bot):
 
             add_order(
                 user_id=callback.from_user.id,
-                product=item['product'],
+                product=item['name'],
                 quantity=quantity,
                 address=data.get('address', 'Не указан!'),
                 comment=data.get('comment', ''),
