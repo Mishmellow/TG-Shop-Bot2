1b560f75c28371077358100d4d31ee2f614fc9d5
Misha <guglmit599@gmail.com>
2025-11-01 12:40:41 +0200

    chore: temporary empty commit for index cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# TG-Shop-Bot1
-Telebot with aiogram3
--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv('BOT_TOKEN')
-DB_PATH = os.getenv('DB_PATH', 'bot.db')
