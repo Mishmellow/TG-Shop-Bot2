d36f1542ea86d8d6ba34cda0309792b93e4b7b8f
Misha <guglmit599@gmail.com>
2025-11-24 20:46:41 +0200

    Update config, add logging and fix bot token

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
+import logging
 from dotenv import load_dotenv
 
+logging.basicConfig(level=logging.INFO)
 load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+if not TOKEN:
+    logging.critical('❌ Критическая ошибка: Переменная BOT_TOKEN не найдена. Проверьте ваш файл .env!')
+    raise ValueError("Bot token is missing!")
 
 TOKEN = os.getenv('BOT_TOKEN')
 DB_PATH = os.getenv('DB_PATH', 'bot.db')
