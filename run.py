54abba7126dae86f8b63bccc9fafa2c1bedcb1f3
Misha <guglmit599@gmail.com>
2025-11-25 22:46:56 +0200

    Update our PORT

--- run.py
+++ run.py
@@ -24,8 +24,8 @@ WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 env_port = os.environ.get("PORT")
 if not env_port:
     logger.warning(
-        "⚠️ Переменная окружения PORT не установлена. Используется порт по умолчанию (8000) для локального режима.")
-    PORT = 8000
+        "⚠️ Переменная окружения PORT не установлена. Используется порт по умолчанию (9000) для Railway.")
+    PORT = 9000
 else:
     PORT = int(env_port)
 
